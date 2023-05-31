2d0852167d8e6035c9c6bcf48786ff48b43150bc
taylor930422 <125858598+taylor930422@users.noreply.github.com>
2023-05-31 17:44:17 +0900

    refactor: [박태현] 불필요한 코드 제거하기

--- src/main/java/com/jgj/byl_process/domain/boards/qna/repository/QnaBoardRepository.java
+++ src/main/java/com/jgj/byl_process/domain/boards/qna/repository/QnaBoardRepository.java
@@ -4,13 +4,10 @@ import com.jgj.byl_process.domain.boards.qna.entity.QnaBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 public interface QnaBoardRepository extends JpaRepository<QnaBoard, Long> {
-
-    QnaBoard findFirstByOrderByQnaBoardIdDesc();
-
+    
     @Query("SELECT q FROM QnaBoard q WHERE q.writer = :writer")
     List<QnaBoard> findByWriter(@Param("writer") String writer);
 }
