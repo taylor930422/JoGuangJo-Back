c757047c057507eab06db7469562ad92b7a10520
taylor930422 <125858598+taylor930422@users.noreply.github.com>
2023-05-31 17:43:12 +0900

    refactor: [박태현] 불필요한 import문 제거 및 log.info 코드 정리

--- src/main/java/com/jgj/byl_process/domain/boards/qna/controller/QnaBoardController.java
+++ src/main/java/com/jgj/byl_process/domain/boards/qna/controller/QnaBoardController.java
@@ -2,17 +2,14 @@ package com.jgj.byl_process.domain.boards.qna.controller;
 
 
 import com.jgj.byl_process.domain.boards.qna.controller.dto.request.QnaBoardRequest;
-import com.jgj.byl_process.domain.boards.qna.controller.dto.response.QnaBoardImgResponse;
 import com.jgj.byl_process.domain.boards.qna.controller.dto.response.QnaBoardListResponse;
 import com.jgj.byl_process.domain.boards.qna.controller.dto.response.QnaBoardModifyResponse;
 import com.jgj.byl_process.domain.boards.qna.controller.dto.response.QnaBoardReadResponse;
-import com.jgj.byl_process.domain.boards.qna.entity.QnaBoard;
 import com.jgj.byl_process.domain.boards.qna.service.QnaBoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,53 +24,38 @@ public class QnaBoardController {
     }
 
     @PostMapping(value = "/register",
-            consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public void qnaBoardRegister(
             @RequestPart(value = "imageFileList", required = false) List<MultipartFile> imageFileList,
-            @RequestPart(value = "qnaInfo") QnaBoardRequest qnaBoardRequest ) {
-        log.info("qnaBoardRegister()");
-
-        if (imageFileList == null) {
-            imageFileList = new ArrayList<>();
-            log.info("널이냐?");
-        }
+            @RequestPart(value = "qnaInfo") QnaBoardRequest qnaBoardRequest) {
 
         qnaBoardService.register(imageFileList, qnaBoardRequest);
     }
 
     @GetMapping("/list")
-    public List<QnaBoardListResponse> qnaBoardList () {
-        log.info("QuestionList() 동작");
+    public List<QnaBoardListResponse> qnaBoardList() {
 
         return qnaBoardService.list();
     }
 
     @GetMapping("/{qnaBoardId}")
     public QnaBoardReadResponse questionBoardRead(@PathVariable("qnaBoardId") Long qnaBoardId) {
-            System.out.println("read() 동작 : " + qnaBoardId);
-            return qnaBoardService.read(qnaBoardId);
 
+        return qnaBoardService.read(qnaBoardId);
     }
 
-@PutMapping(value="/{qnaBoardId}",
-        consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
-public List<QnaBoardModifyResponse> qnaBoardModify(
-        @PathVariable("qnaBoardId") Long qnaBoardId,
-        @RequestPart(value = "imageFileList", required = false) List<MultipartFile> imageFileList,
-        @RequestPart(value = "qnaInfo") QnaBoardRequest qnaBoardRequest ) {
-    log.info("qnaBoardRegister()");
+    @PutMapping(value = "/{qnaBoardId}",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public List<QnaBoardModifyResponse> qnaBoardModify(
+            @PathVariable("qnaBoardId") Long qnaBoardId,
+            @RequestPart(value = "imageFileList", required = false) List<MultipartFile> imageFileList,
+            @RequestPart(value = "qnaInfo") QnaBoardRequest qnaBoardRequest) {
 
-    if (imageFileList == null) {
-        imageFileList = new ArrayList<>();
-        log.info("널이냐?");
+        return qnaBoardService.modify(qnaBoardId, imageFileList, qnaBoardRequest);
     }
-
-    return  qnaBoardService.modify(qnaBoardId, imageFileList, qnaBoardRequest);
-}
 
     @DeleteMapping("/{qnaBoardId}")
     public void questionBoardRemove(@PathVariable("qnaBoardId") Long qnaBoardId) {
-        log.info("\n" + "questionBoardRemove() 동작 : " + qnaBoardId);
 
         qnaBoardService.remove(qnaBoardId);
     }
